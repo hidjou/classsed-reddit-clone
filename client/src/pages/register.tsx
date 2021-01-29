c5335c09355680b288c31e25391f40aba527bb09
Ahmed <ahmed@hadjou.com>
2021-01-29 17:45:27 +0000

    Updated bricks image path and added github action

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -48,7 +48,9 @@ export default function Register() {
 
       <div
         className="h-screen bg-center bg-cover w-36"
-        style={{ backgroundImage: "url('/images/bricks.jpg')" }}
+        style={{
+          backgroundImage: `url('${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/images/bricks.jpg')`,
+        }}
       ></div>
       <div className="flex flex-col justify-center pl-6">
         <div className="w-70">
